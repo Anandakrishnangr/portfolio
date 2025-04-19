8562a4f72a4d3733cac0d13c71c61b576e6e3b48
anandakrishnangr <granandakrishnan@gmail.com>
2025-04-19 15:41:49 +0530

    next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (isServer) {
+      // Prevent pdfjs-dist from trying to load the node‑canvas package
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      }
+    }
+    return config
+  },
 };
 
 export default nextConfig;
